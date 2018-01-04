d933259a8ba8e14c763bc01b8f7e1f2bd7a9afe7
Phillip Maluta <phillip.maluta@volia-software.com>
2018-01-04 13:25:18 +0200

    pass auth without PIN; redirect URL used

--- NestApiModel/AuthWebViewController.swift
+++ NestApiModel/AuthWebViewController.swift
@@ -10,8 +10,6 @@ import UIKit
 
 class AuthWebViewController: UIViewController, UIWebViewDelegate {
     var webView : UIWebView?
-    var pinViewController : PinViewController?
-    var gotPin : ((String)->Void)?
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -19,12 +17,6 @@ class AuthWebViewController: UIViewController, UIWebViewDelegate {
         webView = UIWebView(frame: CGRect(x: 0, y: 0, width: self.view.frame.size.width, height: self.view.frame.size.height))
         webView?.delegate = self
         self.view.addSubview(webView!)
-        let bundle = Bundle(for: type(of: self))
-        pinViewController = PinViewController(nibName: "PinViewController", bundle: bundle)
-        guard gotPin != nil else {
-            return
-        }
-        pinViewController?.gotPin = gotPin
         webView?.loadRequest(URLRequest(url:URL(string: "https://home.nest.com/login/oauth2?client_id=f2a47dec-fb5d-4dad-8370-97a915ab3eb8&state=STATE")!))
     }
 
@@ -33,17 +25,10 @@ class AuthWebViewController: UIViewController, UIWebViewDelegate {
     }
     
     func webView(_ webView: UIWebView, shouldStartLoadWith request: URLRequest, navigationType: UIWebViewNavigationType) -> Bool {
+        print(request.url!)
         return true
     }
     
-    func webViewDidFinishLoad(_:UIWebView){
-        let btnBarNext = UIBarButtonItem.init(title: "Enter PIN", style: .plain, target: self, action: #selector(showPinField))
-        self.navigationItem.rightBarButtonItem = btnBarNext
-    }
-    
-    @objc func showPinField(){
-        self.navigationController?.pushViewController(pinViewController!, animated: true)
-    }
     
     /*
     // MARK: - Navigation
